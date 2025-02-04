a2122529d54806993d30407f45e6f1008024ac84
crystal-delivered <crystal@delivered.co.kr>
2025-02-05 07:12:59 +0900

    초기설정추가

---
+++ src/main/java/com/example/springbootpratice/SpringbootPraticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPraticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPraticeApplication.class, args);
+    }
+
+}
